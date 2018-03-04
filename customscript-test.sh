4138ebe66c8fa65bf5dd2f197f3395ad2378e57b
Johan Burati <johan.burati@gmail.com>
2018-03-04 10:53:01 +0900

    update

--- customscript-test.sh
+++ customscript-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # CustomScript: Create a file in /tmp with a time stamp
 #
-# az vm extension set --resource-group $rg --vm-name $vm --name customScript --publisher Microsoft.Azure.Extensions `
+# az vm extension set --resource-group $rg --vm-name $vm --name customScript --publisher Microsoft.Azure.Extensions \
 # --settings '{"fileUris": ["https://raw.githubusercontent.com/johanburati/ms/master/customscript-test.sh"],"commandToExecute": "./customscript-test.sh"}'
 #
 touch /tmp/customscript-$(date +"%H%M%S")
